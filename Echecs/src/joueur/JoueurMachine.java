8d141f9ce2b169efa9b05341605386dcc3cad363
Manoel <manoel.nunesborba@yahoo.com>
2021-05-03 18:29:00 +0200

    Ajout d'une petite intelligence à la machine (maintenant si le roi est disponible, et le mange !)

--- Echecs/src/joueur/JoueurMachine.java
+++ Echecs/src/joueur/JoueurMachine.java
@@ -12,33 +12,57 @@ public class JoueurMachine extends Joueur {
 
 	@Override
 	public void jouer(Plateau plat, Joueur j) {
+		boolean dejajoue=false;
 		boolean reussi=false;
 		boolean reussi2=false;
-		Random rand = new Random();
-        int i = rand.nextInt(super.getPieces().length);
-        while (!super.getPiecea(i).EstVivante()) {
-        	i = rand.nextInt(super.getPieces().length);
-        }
-        	
-        	do {
-            	int x = rand.nextInt(8);
-                int y = rand.nextInt(8);
-    		if(super.getPiecea(i).estPossible(x,y,j)) {
-    			System.out.println("La pièce" + super.getPiecea(i).toString() + " a bougé en X : " + x + " Et Y : " + y);
+		int roi = chercheRoi(j);
+		for(int a=0;a<super.getPieces().length;++a) {
+			if(super.getPiecea(a).estPossible(j.getPiecea(roi).getX(), j.getPiecea(roi).getY(), j)) {
+				System.out.println("La pièce" + super.getPiecea(a).toString() + " a bougé en X : " + j.getPiecea(roi).getX() + " Et Y : " + j.getPiecea(roi).getY());
+				int x = j.getPiecea(roi).getX();
+				int y = j.getPiecea(roi).getY();
     			
-    			plat.cls(super.getPiecea(i).getX(),super.getPiecea(i).getY());
+    			plat.cls(super.getPiecea(a).getX(),super.getPiecea(a).getY());
+    			dejajoue=true;
     			super.EtreMangé(x,y, j);
-    			super.getPiecea(i).setXY(x, y);
-    			plat.setTab(super.getPiecea(i));
+    			super.getPiecea(a).setXY(x, y);
+    			plat.setTab(super.getPiecea(a));
     			reussi=true;
     			if(j.RestePiece()) {
     				super.setTour(false);
     			}else {
     				super.setTour(true);
     				}
-    			
-    		}
-            }while(!reussi);
+			}
+		}
+		if(!dejajoue) {
+			Random rand = new Random();
+	        int i = rand.nextInt(super.getPieces().length);
+	        while (!super.getPiecea(i).EstVivante()) {
+	        	i = rand.nextInt(super.getPieces().length);
+	        }
+	        	
+	        	do {
+	            	int x = rand.nextInt(8);
+	                int y = rand.nextInt(8);
+	    		if(super.getPiecea(i).estPossible(x,y,j)) {
+	    			System.out.println("La pièce" + super.getPiecea(i).toString() + " a bougé en X : " + x + " Et Y : " + y);
+	    			
+	    			plat.cls(super.getPiecea(i).getX(),super.getPiecea(i).getY());
+	    			super.EtreMangé(x,y, j);
+	    			super.getPiecea(i).setXY(x, y);
+	    			plat.setTab(super.getPiecea(i));
+	    			reussi=true;
+	    			if(j.RestePiece()) {
+	    				super.setTour(false);
+	    			}else {
+	    				super.setTour(true);
+	    				}
+	    			
+	    		}
+	            }while(!reussi);
+		}
+		
         	try {
         		  Thread.sleep(500);
         		} catch (InterruptedException e) {e.printStackTrace();}
